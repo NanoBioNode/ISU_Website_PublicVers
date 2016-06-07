f7154bc7a816ab40cfe7a226d1623063c89de221
itg-abby <abbysobh@gmail.com>
2016-06-07 11:48:36 -0500

    Add files via upload

---
+++ jsonDataPages/Time24GeneOPY1MetabTotal_FA.php
@@ -0,0 +1,8 @@
+<?php 
+header('Content-Type: application/json');
+// you should get params from url
+//
+
+$data = file_get_contents("http://www.metnetdb.org/PMR/api_v1/getResultCoanalysis.php?expid=54d595cce9b6710f2c9658ee&pid=54d595cce9b6710f2c9658f0&mid=54d5a645e9b6712c503bad09&genename=OPY1&hours=24");
+echo $data;
+?>
